fe54b0ec63d2855d6d3260277196d164c5442b1d
csigrist@umass.edu <csigrist@umass.edu>
2024-09-30 14:49:57 -0400

    did connect4

--- connect4.py
+++ connect4.py
@@ -6,7 +6,7 @@
 #                                                                                                                                                                         #
 ########################################################################################################################################################################### 
 
-def print_board(baord):
+def print_board(board):
     '''
     This function will take in a board state and then print it in a human-legible format. Make sure that you use distinguishable characters for each player
     and that the coloumns and rows are displayed correctly
@@ -21,14 +21,47 @@ def print_board(baord):
     ## TODO Implement a print of the 2d array with unique characters for each player 
     ############################################
 
+    for row in board:
+        print_row(row)
+
+    print("-----------------------")
+    print(" 1  2  3  4  5  6  7   ")
 
 
     ############################################
 
-
     return None
 
-def game_over(board):
+def print_row(row):
+    str = ""
+    for elem in row:
+        if elem == 1:
+            str += "[X]"
+        elif elem == -1:
+            str += "[O]"
+        elif elem == 0:
+            str += "[ ]"
+    print(str)
+
+def n_in_a_row(board, row, col, dir=(0,1), n=4):
+
+    end_loc = (row+(n*dir[0]), col+(n*dir[1]))
+
+    if end_loc[0] > 5 or end_loc[0] < 0 or end_loc[1] > 6 or end_loc[1] < 0:
+        return False
+
+    if n <= 1:
+        return True
+
+    match_next = board[row][col] == board[row+dir[0]][col+(dir[1])]
+
+    if n <= 2:
+        return match_next
+    
+    else:
+        return match_next and n_in_a_row(board, row+dir[0], col+(dir[1]), dir=dir, n=n-1)
+
+def game_over(board, row, col):
     '''
     Parameters:
         board: A 2d (6x7) list (of lists) which represents a certain state of the board
@@ -41,11 +74,16 @@ def game_over(board):
     ## TODO Implement check of a 4 in a row
     ############################################
 
+    for x in [-1, 0, 1]:
+        for y in [-1,0,1]:
+            if not (x == 0 and y == 0):
+                if n_in_a_row(board, row, col, (x,y), n=4):
+                    return board[row][col]
 
 
     ############################################
 
-    return winner
+    return False
 
 
 def valid_move(board, col):
@@ -62,13 +100,15 @@ def valid_move(board, col):
     ## TODO Implement check of a full coloumn
     ############################################
 
+    if board[0][col] != 0:
+        return False
+    else:
+        return True
 
 
     ############################################
-    
-    return valid
 
-def move(board, player, col):
+def move(board, player):
     '''
     This function should ask the current player which move they'd like to make and then implement it if possible, else
     ask them to give a different move
@@ -77,19 +117,24 @@ def move(board, player, col):
         board: A 2d (6x7) list (of lists) which represents a certain state of the board
                values of 1 represent player 1's piecces, a value of -1 represents player 2's and 0 represents empty
         player: An Int representing the player (Player 1 -> 1, Player 2 -> -1)
-        col: The coloumn being played in 
     Returns:
         new_board: A 2d list representing the updated board after making the move
     '''
         
     ## TODO Implement a move (with check)
     ############################################
-
-
+    col_choice = int(input("which col would you like to play in?")) - 1
+    while not valid_move(board, col_choice):
+        col_choice = input("Not a valid move, which col would you like to play in?")
+    
+    for i in range(6):
+        if i == 5 or board[i+1][col_choice] != 0 :
+            board[i][col_choice] = player
+            break
 
     ############################################
         
-    return new_board
+    return board, (i, col_choice)
 
 def play_game():
     '''
@@ -103,10 +148,23 @@ def play_game():
     ## TODO The full game (using all previous functions)
     ############################################
 
+    player = 1
 
+    board = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+
+    while True:
+        print_board(board)
+        print("Player", (player+3)%3, "("+("X" if player==1 else "O")+")", "your turn" )
+        board, (row, col) = move(board, player)
+        winner = game_over(board, row, col)
+        if winner != 0:
+            print_board(board)
+            print("CONGRATS PLAYER", int((winner+3)%3), "! YOU WIN BECAUSE PLAYER", int(((winner*-1)+3)%3), "SUCKS!")
+            return winner
+        player *= -1
+        
+        
 
     ############################################
-
-    return None
 
 play_game()
